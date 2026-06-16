6e409640451dded627f820546b0ea3319c5c7247
hmduongdl <hoanglong.workdl@gmail.com>
2026-06-16 11:05:43 +0700

    fix: update user retrieval logic to allow login with email or username

--- src/auth.ts
+++ src/auth.ts
@@ -28,8 +28,13 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
           return null;
         }
 
-        const user = await db.user.findUnique({
-          where: { username },
+        const user = await db.user.findFirst({
+          where: {
+            OR: [
+              { username: username },
+              { email: username }
+            ]
+          },
         });
 
         if (user && user.password) {
